dea80afb0f634b41b529518180db34aa03e704a2
AlexUa <ualexzt@gmail.com>
2021-10-30 21:29:02 +0300

    add config db and api

--- apps/backend/api/src/app/app.module.ts
+++ apps/backend/api/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import {Module} from '@nestjs/common'
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import {environment} from '../environments/environment'
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    ...environment.connection
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
